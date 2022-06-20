e9792d56be41c6c2c89b61f1a5943438dd66fb5b
Toshkumar Sharma <tsharma@c1r2p1.42nice.fr>
2022-06-20 18:35:41 +0200

    Modified code to have the outline of get_next_line in place.

--- get_next_line.c
+++ get_next_line.c
@@ -3,25 +3,103 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: toshsharma <toshsharma@student.42.fr>      +#+  +:+      +#+        */
+/*   By: tsharma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/30 12:03:25 by toshsharma        #+#    #+#             */
-/*   Updated: 2022/05/30 16:57:00 by toshsharma       ###   ########.fr       */
+/*   Created: 2022/06/20 12:48:58 by tsharma           #+#    #+#             */
+/*   Updated: 2022/06/20 18:29:46 by tsharma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <fcntl.h>
 #include <stdio.h>
 
-char    *get_next_line(int fd)
+int	init(void)
 {
-    int         read_count;
-    static char *buff;
-
-    buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    read_count = read(fd, buff, BUFFER_SIZE + 1);
-    printf("%d", read_count);
-    printf("%s", buff);
-    return (buff);
+	static int	count = 0;
+	count++;
+	return (count);
 }
 
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*res;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+		{
+			res = (char *)&s[i];
+			return (res);
+		}
+		i++;
+	}
+	if (s[i] == (char)c)
+	{
+		res = (char *)&s[i];
+		return (res);
+	}
+	return (NULL);
+}
+
+void	send_line(t_list **list)
+{
+}
+
+void	break_line
+
+void	traverse_file(int fd, t_list **list)
+{
+	char	*buff;
+	int		read_count;
+	char	*extra;
+	char	*line;
+
+	while (read_count != 0)
+	{
+		buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		read_count = read(fd, buff, BUFFER_SIZE + 1);
+		printf("Read count is %d\n", read_count);
+		printf("Buff output is %s\n", buff);
+		if (ft_strchr(buff, '\n') == 1)
+		{
+			break_line(line, extra, buff);
+			ft_lstadd_back(list, ft_lstnew(line));
+		}
+		// Above break line is made to store extra in extra, add the necessary
+		// part to line (along with the existing stuff)
+		// and to have the required length of the code.
+	}
+}
+
+void	get_next_line(int fd)
+{
+	t_list	**list;
+
+	if (init() == 1)
+		traverse_file(fd, list);
+	send_line(list);
+}
+
+int	main(void)
+{
+	int		fd;
+	char	*res;
+
+	fd = open("test.txt", O_RDONLY);
+	if (fd != -1)
+	{
+		get_next_line(fd);
+		get_next_line(fd);
+		get_next_line(fd);
+		get_next_line(fd);
+		get_next_line(fd);
+		get_next_line(fd);
+	}
+	close(fd);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -3,24 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: toshsharma <toshsharma@student.42.fr>      +#+  +:+       +#+        */
+/*   By: tsharma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/30 12:03:55 by toshsharma        #+#    #+#             */
-/*   Updated: 2022/05/31 14:41:40 by toshsharma       ###   ########.fr       */
+/*   Created: 2022/06/20 15:41:56 by tsharma           #+#    #+#             */
+/*   Updated: 2022/06/20 16:54:58 by tsharma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+#endif
 
-char    *get_next_line(int fd);
-char	*ft_strchr(const char *s, int c);
-size_t	ft_strlen(const char *s);
-char	*ft_strdup(char *src);
-char	*ft_strjoin(char const *s1, char const *s2);
+typedef struct	s_list
+{
+	char			*content;
+	struct s_list	*next;
+}	t_list;
 
-#endif 
-                       
+void	get_next_line(int fd);
+
+#endif
