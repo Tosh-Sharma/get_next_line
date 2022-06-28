beb7c35cec9cbf76c24aac0ee7761a8e39418c7c
Toshkumar Sharma <tsharma@c1r7p1.42nice.fr>
2022-06-28 18:46:49 +0200

    Loads of progress. need to fix the cpy_and_erase_src function.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tsharma <tsharma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 12:48:58 by tsharma           #+#    #+#             */
-/*   Updated: 2022/06/27 16:20:58 by tsharma          ###   ########.fr       */
+/*   Updated: 2022/06/28 17:17:40 by tsharma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@
 char	*append_line(char *buffer, char *line, char *extra)
 {
 	char	*new_line;
-
+//	printf("line is \"%s\"\n", line);
+//	printf("extra is \"%s\"\n", extra);
 	if (extra != NULL)
 		new_line = ft_strjoin(extra, line);
 	if (buffer != NULL)
 		new_line = ft_strjoin(line, buffer);
+//	printf("new_line is \"%s\"\n", new_line);
 	return (new_line);
 }
 
@@ -31,6 +33,7 @@ char	*cpy_and_erase_src(char *dst, char *src)
 
 	i = 0;
 	n = ft_strlen(src);
+	printf("src is %s\n", src);
 	dst = (char *)malloc(sizeof(char) * (n + 1));
 	while (src[i] != '\0')
 	{
@@ -61,6 +64,8 @@ char	*break_line(char *buffer, char *line, char *extra, int position)
 		i++;
 	}
 	temp[i] = '\0';
+//	printf("line is \"%s\"\n", line);
+//	printf("temp is \"%s\"\n", temp);
 	new_line = ft_strjoin(line, temp);
 	free(line);
 	j = 0;
@@ -70,6 +75,7 @@ char	*break_line(char *buffer, char *line, char *extra, int position)
 		j++;
 	}
 	extra[j] = '\0';
+//	printf("break_line is %s", new_line);
 	return (new_line);
 }
 
@@ -79,7 +85,7 @@ char	*break_multi_lines(char *line, char *extra, int position)
 	int		i;
 
 	i = 0;
-	new_line = malloc(sizeof(char) * (ft_strlen(line) - position));
+	new_line = malloc(sizeof(char) * (ft_strlen(line) - position + 2));
 	while (i <= position)
 	{
 		new_line[i] = line[i];
@@ -87,9 +93,9 @@ char	*break_multi_lines(char *line, char *extra, int position)
 	}
 	new_line[i] = '\0';
 	i = 0;
-	while (i <= ft_strlen(line - position))
+	while (i <= (ft_strlen(line) - position))
 	{
-		extra[i] = line[position + i];
+		extra[i] = line[position + 1 + i];
 		i++;
 	}
 	extra[i] = '\0';
@@ -107,21 +113,16 @@ char	*get_next_line(int fd)
 
 	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	line = cpy_and_erase_src(line, extra);
-	while (1)
+//	printf("line before code starts is \"%s\"\n", line);
+	read_count = -1;
+	while (read_count != 0)
 	{
 		if (ft_strchr(line, '\n') != -1)
-		{
-			line = break_multi_lines(line, extra, ft_strchr(line, '\n'));
-			return (line);
-		}
+			return (break_multi_lines(line, extra, ft_strchr(line, '\n')));
 		read_count = read(fd, buffer, BUFFER_SIZE + 1);
-		printf("Buffer is \"%s\"\n", buffer);
 		position = ft_strchr(buffer, '\n');
 		if (position != -1)
-		{
-			line = break_line(buffer, line, extra, position);
-			return (line);
-		}
+			return (break_line(buffer, line, extra, position));
 		else
 			line = append_line(buffer, line, extra);
 	}
